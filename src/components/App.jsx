498607e04a11d3ab3dbf51021bec16bfb6714dd0
IlliaBekarevych <93090373+IlliaBekarevych@users.noreply.github.com>
2022-07-11 15:58:34 +0300

    add hw-02-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  onSubmit = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+  checkNewContact = newContact => {
+    if (
+      this.state.contacts.find(
+        contact =>
+          contact.name.toLocaleLowerCase() ===
+          newContact.name.toLocaleLowerCase()
+      )
+    ) {
+      alert(newContact.name + ' is alredy in contacts');
+      return true;
+    }
+    return false;
+  };
+  onChangeFilter = async e => {
+    await this.setState({ filter: e.target.value });
+  };
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+  render() {
+    const { contacts, filter } = this.state;
+    const normFilter = filter.toLocaleLowerCase();
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normFilter)
+    );
+
+    return (
+      <div className="main">
+        <h1>Phonebook</h1>
+        <ContactForm
+          onSubmit={this.onSubmit}
+          checkNewContact={this.checkNewContact}
+        />
+        <h2>Contacts</h2>
+        <Filter filter={filter} onChange={this.onChangeFilter} />
+        <ContactList
+          visibleContacts={visibleContacts}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
